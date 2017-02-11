9d8134f28dd6ce8e19e13438ec216d244d0bea9d
Ali H <ahasan.ali@gmail.com>
2017-02-11 19:16:51 +0500

    3- hot module replace basic setup.

--- app/index.js
+++ app/index.js
@@ -1,3 +1,13 @@
 import component from './component';
 
-document.body.appendChild(component());
+let domComponent = component();
+
+document.body.appendChild(domComponent);
+
+if( module.hot ) {
+  module.hot.accept('./component', () => {
+    const nextComponent = component();
+    document.body.replaceChild(nextComponent, domComponent);
+    domComponent = nextComponent;
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const webpack = require('webpack');
 
 const PATHS  = {
   app : path.join(__dirname, 'app'),
@@ -23,8 +23,49 @@ const common = {
   ],
 };
 
-module.exports = function( env ) {
-  console.log("environment:", env);
-
+function production() {
   return common;
 }
+
+function development() {
+  const config = {
+    devServer: {
+        // Enable history API fallback so HTML5 History API baed
+        // routing works. This is a good default that will come
+        // in handy in more complicated setup.
+        historyApiFallback: true,
+
+        // Don't refresh if hot loading fails. If you want
+        // refresh behavior, set hot: true instead.
+        hotOnly: true,
+
+        // Display only errors to reduce the amount of output.
+        stats: 'errors-only',
+
+        // Parse host and port from env to allow customizations.
+        //
+        host: process.env.HOST, // Defaults to `localhost`
+        port: process.env.PORT, // Defaults to 8080
+    },
+    plugins:[
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NamedModulesPlugin(),
+    ]
+  };
+
+  return Object.assign(
+    {},
+    common,
+    config,
+    {
+      plugins: common.plugins.concat(config.plugins)
+    }
+  );
+}
+
+
+module.exports = function( env ) {
+  if( env === 'production')
+    return production();
+  return development();
+};
