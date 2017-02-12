beab6d55f900be33afc0e8c8dd83d91db95c8558
Ali H <ahasan.ali@gmail.com>
2017-02-12 15:35:51 +0500

    4- added linting support using eslint.

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const webpack = require('webpack');
 const PATHS  = {
   app : path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build'),
-}
+};
 
 const common = {
   entry: {
@@ -33,24 +33,55 @@ function development() {
         // Enable history API fallback so HTML5 History API baed
         // routing works. This is a good default that will come
         // in handy in more complicated setup.
-        historyApiFallback: true,
+      historyApiFallback: true,
 
         // Don't refresh if hot loading fails. If you want
         // refresh behavior, set hot: true instead.
-        hotOnly: true,
+      hotOnly: true,
 
         // Display only errors to reduce the amount of output.
-        stats: 'errors-only',
+      stats: 'errors-only',
 
         // Parse host and port from env to allow customizations.
         //
-        host: process.env.HOST, // Defaults to `localhost`
-        port: process.env.PORT, // Defaults to 8080
+      host: process.env.HOST, // Defaults to `localhost`
+      port: process.env.PORT, // Defaults to 8080
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          enforce: 'pre', // force the webpack to run this loader first.
+
+          loader: 'eslint-loader',
+          options: {
+            emitWarnings: true,
+          },
+        },
+      ],
     },
     plugins:[
       new webpack.HotModuleReplacementPlugin(),
       new webpack.NamedModulesPlugin(),
-    ]
+      new webpack.LoaderOptionsPlugin( {
+        options: {
+          eslint: {
+              // Fail only on errors
+            failOnWarning: false,
+            failOnError: true,
+              // Disable/enable autofix
+            fix:false,
+
+              //output to jenkins compatible xml
+            outputReport: {
+              filePath: 'checkstyle.xml',
+              formatter: require('eslint/lib/formatters/checkstyle'),
+            },
+
+          },
+        },
+      }),
+    ],
   };
 
   return Object.assign(
@@ -58,7 +89,7 @@ function development() {
     common,
     config,
     {
-      plugins: common.plugins.concat(config.plugins)
+      plugins: common.plugins.concat(config.plugins),
     }
   );
 }
